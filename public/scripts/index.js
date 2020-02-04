bae1a5f8e2e9fc0f3d004bec3582e0f88a72528e
Bryan <bwon0003@student.monash.edu>
2020-02-04 15:52:36 +1100

    Refactored shared.js to include consistent functions and naming conventions, split up shared.js into index.js

---
+++ public/scripts/index.js
@@ -0,0 +1,136 @@
+// Scroll animations
+
+const   $INTRO                      = document.querySelector('.intro'),
+        $HERO_IMAGE                 = document.querySelector('.hero-section__image'),
+        $HERO_TEXT                  = document.querySelector('.hero-section__text'),
+        $CTA_ARROW                  = document.querySelector('.scroll-cta__arrow'),
+        $CTA_BUTTON                 = document.querySelector('#scroll-cta__button'),
+        $CHAP_LEFT                  = document.querySelectorAll('.chapters__item--left'),
+        $CHAP_RIGHT                 = document.querySelector('.chapters__item--right'),
+        $CHAP_HEADER                = document.querySelector('.chapters__header'),
+        SCROLL_Y_BREAKPOINTS        = [130, 500, 680, 950, 1190];
+        
+var heroHidden = false;
+
+initIndex();
+
+function initIndex(){
+    // Initialise index page content scroll animaion
+    window.onscroll = () => {
+        initOnScrollContentAnimations();
+        initOnScrollHeaderAnimations();
+    };
+}
+
+function initOnScrollContentAnimations() {
+    /**
+     * initOnScrollContentAnimations initialises animations of DOM elements in the contents of <main> that are to occur as the user scrolls vertically (y-axis) on the web-page.
+    */
+
+    /*
+    As the user scrolls past the 1st breakpoint:
+        - The hero text should fade out.
+    As the user scrolls back to before this breakpoint, elements should return to their initial state.
+    */
+    if (window.scrollY > SCROLL_Y_BREAKPOINTS[0]) {
+        $HERO_TEXT.style.opacity = (1-(window.scrollY/250));
+    } else {
+        $HERO_TEXT.style.opacity = 1;
+    }
+
+    /*
+    As the user scrolls past the 2nd breakpoint:
+        - The call to action button in the hero section should fade out.
+        - The hero image should fade out.
+    As the user scrolls back to before this breakpoint, elements should return to their initial state.
+    */
+    if (window.scrollY > SCROLL_Y_BREAKPOINTS[1]) {
+        $CTA_ARROW.style.opacity = 0;
+        $CTA_BUTTON.style.opacity = (1-(window.scrollY/700));
+        $HERO_IMAGE.style.opacity = (1-(window.scrollY/700));
+        heroHidden = true;
+    } else {
+        $CTA_ARROW.style.opacity = 1;
+        $CTA_BUTTON.style.opacity = 1;
+        $HERO_IMAGE.style.opacity = 1;
+        heroHidden = false;
+    }
+
+    /*
+        As the user scrolls past the 3rd breakpoint:
+            - The first chapter should slide in from the left.
+        As the user scrolls back to before this breakpoint, the chapter item should stay in its final state.
+    */
+    if (window.scrollY > SCROLL_Y_BREAKPOINTS[2]) {
+        $CHAP_LEFT[0].classList.add('slide-in');
+    }
+    /*
+        As the user scrolls past the 4th breakpoint:
+            - The second chapter should slide in from the right.
+        As the user scrolls back to before this breakpoint, the chapter item should stay in its final state.
+    */
+    if (window.scrollY > SCROLL_Y_BREAKPOINTS[3]) {
+        $CHAP_RIGHT.classList.add('slide-in');
+    }
+    /*
+        As the user scrolls past the 5th breakpoint:
+            - The third chapter should slide in from the left.
+        As the user scrolls back to before this breakpoint, the chapter item should stay in its final state.
+    */
+    if (window.scrollY > SCROLL_Y_BREAKPOINTS[4]) {
+        $CHAP_LEFT[1].classList.add('slide-in');
+    }
+
+
+    // If the hero section is hidden, fade in content of the intro and chapter header.
+    if (heroHidden) {
+        $INTRO.classList.remove('fade-hide');
+        $INTRO.classList.add('fade-reveal');
+        $CHAP_HEADER.classList.remove('fade-hide');
+        $CHAP_HEADER.classList.add('fade-reveal');
+    }
+}
+
+
+function initOnScrollHeaderAnimations() {
+    /**
+     * initOnScrollHeaderAnimations initialises animations of the header DOM elements that are to occur as the user scrolls vertically (y-axis) on the web-page.
+     * When the user scrolls past the hero section, the brand should change to the original (from the alternate) logo, and the toggle button should change from white to grey.
+     * As the user scrolls back to the hero section, the brand and toggle button should revert to their initial states.
+    */
+
+    // If the hero section is hidden (i.e. has been scrolled past)
+    if (heroHidden) {
+        // Turn header background white
+        $MAIN_HEADER.classList.remove('main-header--transparent');
+        $MAIN_HEADER.classList.add('header__element--white');
+        // Replace alternate logo with original logo that does not have hover effects.
+        $LOGO_ALT.classList.remove('image_on');
+        $LOGO_ALT.classList.add('brand-alt-hover--disable');
+        $LOGO.classList.remove('image_off');
+        $LOGO.classList.add('brand-hover--disable');
+        // Make toggle button bars grey
+        $TOGGLE_BTN_BARS.forEach((bar) => {
+            bar.classList.remove('header__element--white');
+            bar.classList.add('header__element--grey');
+        });
+
+
+    }
+    // If the hero section is not hidden
+    if (!heroHidden) {
+        // Turn header background transparent
+        $MAIN_HEADER.classList.remove('header__element--white');
+        $MAIN_HEADER.classList.add('main-header--transparent');
+        // Replace original logo with alternate (white-only) logo that has hover effects.
+        $LOGO.classList.remove('brand-hover--disable');
+        $LOGO.classList.add('image_off');
+        $LOGO_ALT.classList.remove('brand-alt-hover--disable');
+        $LOGO_ALT.classList.add('image_on');
+        // Make toggle button bars white
+        $TOGGLE_BTN_BARS.forEach((bar) => {
+            bar.classList.remove('header__element--grey');
+            bar.classList.add('header__element--white');
+        });
+    }
+}
--- public/scripts/shared.js
+++ public/scripts/shared.js
@@ -1,212 +1,165 @@
-var toggleButton = document.querySelector('.toggle-button');
-var toggleButtonClicked = false;
-var mobileNav = document.querySelector('.mobile-nav');
-var mobileNavBg = document.querySelector('.mobile-nav__background');
-var mobileNavItems = document.querySelector('.mobile-nav__items');
-var logo = document.querySelector('#logo');
-var logoAlt = document.querySelector('#logo--alt')
-var mainHeader = document.querySelector('.main-header');
-var intro = document.querySelector('.intro');
-var brand = document.querySelector('.brand');
-var introOffset = offset(intro);
-var heroImage = document.querySelector('.hero-section__image');
-var heroText = document.querySelector('.hero-section__text');
-var html = document.querySelector('html');
-var ctaArrow = document.querySelector('.scroll-cta__arrow');
-var ctaButton = document.querySelector('#scroll-cta__button');
-var chapLeft = document.querySelectorAll('.chapter-left');
-var chapRight = document.querySelector('.chapter-right');
-var chapHeader = document.querySelectorAll('.chapter-header');
-var copyrightYear = document.querySelector('#copyright-year');
-var currentDate = new Date();
-var currentYear = currentDate.getFullYear();
-var menuLabel = document.querySelector('#menu__label');
+// $ prefix to indicate it is a DOM element
+// Toggle button
+const   $TOGGLE_BTN                 = document.querySelector('.toggle-button'),
+        $TOGGLE_BTN_BARS            = document.querySelectorAll('.toggle-button__bar'),
+        $MENU_LABEL                 = document.querySelector('#menu__label'),
+        $MOBILE_NAV                 = document.querySelector('.mobile-nav'),
+        $MOBILE_NAV_BG              = document.querySelector('.mobile-nav__background'),
+        $MOBILE_NAV_ITEMS           = document.querySelector('.mobile-nav__items'),
+        $LOGO                       = document.querySelector('#logo'),
+        $LOGO_ALT                   = document.querySelector('#logo--alt'),
+        $MAIN_HEADER                = document.querySelector('.main-header'),
+        $BRAND                      = document.querySelector('.brand'),
+        $BODY                       = document.querySelector('body');
+var toggleBtnClicked = false;
 
-copyrightYear.textContent = currentYear;
+init();
 
-const mainHeaderHeight = 70.4;
-const toggleButtonBars = document.querySelectorAll('.toggle-button__bar')
-var toggleButtonBar1 = toggleButtonBars[0];
-var toggleButtonBar2 = toggleButtonBars[1];
-var toggleButtonBar3 = toggleButtonBars[2];
-
-
-var heroHidden = false;
-
-
-toggleButton.addEventListener('click', () => {
-    if (!toggleButtonClicked) {
-        mobileNav.classList.remove('mobile-nav__hidden');
-        mobileNav.classList.add('mobile-nav__reveal');
-        setTimeout(() => {
-            // Need to do background transition AFTER the mobile-nav is revealed (i.e. has its display changed from none to block), otherwise the transition will not execute.
-            mobileNavBg.classList.add('mobile-nav__background--open');  
-            mainHeader.classList.add('main-header--transparent');
-            mainHeader.classList.remove('header__element--white');
-            logoAlt.classList.remove('brand-alt-hover--disable');
-            logoAlt.classList.add('image_on');
-            logo.classList.remove('brand-hover--disable');
-            logo.classList.add('image_off');
-            // reset header to state it was at top of page as when we click on toggle it brings user bcak to top of page
-            mobileNavItems.classList.add('mobile-nav__items--reveal');
-        }, 100);
-
-        // Only change logos, and allow us to click on the toggle button again AFTER it has finished loading (to prevent possible bug where user spams on button)
-        // setTimeout(() => {
-        //     toggleButton.style.pointerEvents = 'auto';
-        // }, 1000)
-
-        brand.classList.add('hidden');
-        brand.classList.add('unclickable');
-        toggleButtonBars.forEach((bar) => {
-            bar.classList.add('header__element--grey');
-            bar.classList.remove('header__element--white');
-        });
-        toggleButtonBar1.classList.add('toggle-button__bar-1--clicked');
-        toggleButtonBar2.classList.add('toggle-button__bar-2--clicked');
-        toggleButtonBar3.classList.add('toggle-button__bar-3--clicked');
-
-        menuLabel.textContent = "CLOSE";
-
-        toggleButtonClicked = true;
-
-        // mobileNav.style.overflow = 'hidden';
-
-        // To prevent scrolling on the body while the mobile nav is open, there are several possible solutions. 
-        // (1) Change hte position of the body to 'fixed'.
-        // (2) Change the overflow of the body to "hidden".
-        // However, these solutions will automatically scroll back up to the top of hte page, and neither of these will allow us to animate the transition of scrolling back up as they are binary options.
-        // We can set position of the body to absolute instead.
-        document.querySelector('body').style.position = 'fixed';
-    } else {
-        mobileNavItems.classList.remove('mobile-nav__items--reveal');
-        mobileNavItems.classList.add('mobile-nav__items--hide');
-        mobileNavBg.classList.add('mobile-nav__background--close');
-        mobileNavBg.classList.remove('mobile-nav__background--open');
-        setTimeout(() => {
-            // Need to hide (i.e. set display to none) AFTER the transition is done.
-            mobileNav.classList.remove('mobile-nav__reveal');
-            mobileNav.classList.add('mobile-nav__hidden');
-            mobileNavBg.classList.remove('mobile-nav__background--close');
-            mobileNavItems.classList.remove('mobile-nav__items--hide');
-        }, 500);
-
-        document.querySelector('body').style.position = 'static';
-        brand.classList.remove('hidden');
-        brand.classList.remove('unclickable');
-        toggleButtonBars.forEach((bar) => {
-            // Only keep header element as grey if the hero is hidden (i.e. the user has scrolled to that point)
-            // Else, make header element white.
-            if (!heroHidden) {
-                bar.classList.remove('header__element--grey');
-                bar.classList.add('header__element--white');
-            }
-        });
-        toggleButtonBar1.classList.remove('toggle-button__bar-1--clicked');
-        toggleButtonBar2.classList.remove('toggle-button__bar-2--clicked');
-        toggleButtonBar3.classList.remove('toggle-button__bar-3--clicked');
-
-        toggleButtonBar1.classList.add('toggle-button__bar-1--clicked-off');
-        toggleButtonBar2.classList.add('toggle-button__bar-2--clicked-off');
-        toggleButtonBar3.classList.add('toggle-button__bar-3--clicked-off');
-
-        // Remove classes after animations
-        setTimeout(() => {
-            toggleButtonBar1.classList.remove('toggle-button__bar-1--clicked-off');
-            toggleButtonBar2.classList.remove('toggle-button__bar-2--clicked-off');
-            toggleButtonBar3.classList.remove('toggle-button__bar-3--clicked-off');
-        }, 500)
-        toggleButtonClicked = false;
-
-        menuLabel.textContent = "MENU";
-    }
-});
-
-
-window.onscroll = () => {
-    if (window.scrollY > 130) {
-        heroText.style.opacity = (1-(window.scrollY/250));
-    } else {
-        heroText.style.opacity = 1;
-    }
-    if (window.scrollY > 500) {
-        ctaArrow.style.opacity = 0;
-        ctaButton.style.opacity = (1-(window.scrollY/700));
-        
-    } else {
-        ctaArrow.style.opacity = 1;
-        ctaButton.style.opacity = 1;
-    }
-    if (window.scrollY > 500) {
-        heroImage.style.opacity = (1-(window.scrollY/700));
-        // heroImage.classList.add('hidden');
-        heroHidden = true;
-        // heroImage.style.opacity = (1-(window.scrollY/750));
-    } else {
-        heroImage.style.opacity = 1;
-        // heroImage.classList.remove('hidden');
-        heroHidden = false;
-    }
-    if (window.scrollY > 680) {
-        chapLeft[0].classList.add('wipe-in');
-    }
-    if (window.scrollY > 950) {
-        chapRight.classList.add('wipe-in');
-    }
-    if (window.scrollY > 1190) {
-        chapLeft[1].classList.add('wipe-in');
-    }
-    // Transitions when the bottom of the header reaches the intro section, rather than when the top reaches it, as the offset calculates how far the intro section is from the top of the window/header.
-    if (heroHidden && !toggleButtonClicked) {
-        mainHeader.classList.remove('main-header--transparent');
-        mainHeader.classList.add('header__element--white');
-        logoAlt.classList.remove('image_on');
-        logoAlt.classList.add('brand-alt-hover--disable');
-        logo.classList.remove('image_off');
-        logo.classList.add('brand-hover--disable');
-        brand.classList.remove('disable-hover');
-
-        chapHeader.forEach((header) => {
-            header.classList.remove('fade-hide');
-            header.classList.add('fade-reveal');
-        });
-        
-        
-
-        toggleButtonBars.forEach((bar) => {
-            bar.classList.remove('header__element--white');
-            bar.classList.add('header__element--grey');
-        });
-
-
-    }
-    if (!heroHidden && !toggleButtonClicked) {
-        mainHeader.classList.remove('header__element--white');
-        mainHeader.classList.add('main-header--transparent');
-        logo.classList.remove('brand-hover--disable');
-        logo.classList.add('image_off');
-        logoAlt.classList.remove('brand-alt-hover--disable');
-        logoAlt.classList.add('image_on');
-
-        chapHeader.forEach((header) => {
-            header.classList.add('fade-hide');
-            header.classList.remove('fade-reveal');
-        });
-
-        brand.classList.add('disable-hover');
-
-        toggleButtonBars.forEach((bar) => {
-            bar.classList.remove('header__element--grey');
-            bar.classList.add('header__element--white');
-        });
-
-        // html.classList.remove('disable-scroll-snapping');
-    }
+function init() {
+    // Initialise header mobile nav bar
+    initToggleBtn();
+    // Initialise footer copyright year
+    let copyrightYear = document.querySelector('#copyright-year');
+    let currentYear = new Date().getFullYear();
+    copyrightYear.textContent = currentYear;
 }
 
-function offset(el) {
-    var rect = el.getBoundingClientRect(),
-    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
-    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+function initToggleBtn() {
+    /**
+     * initToggleBtn initialises the on-click event listeners for the toggle button, which enables users to open or close a mobile menu navigation bar.
+     */
+    $TOGGLE_BTN.addEventListener('click', () => {
+        // If the toggle button is clicked to open the mobile menu
+        if (!toggleBtnClicked) {
+            openMobileMenu();
+            openToggleAnimation();
+            toggleBtnClicked = true;
+        } 
+        // If the toggle button is clicked to close the mobile menu
+        else {
+            closeMobileMenu();
+            closeToggleAnimation();
+            toggleBtnClicked = false;
+        }
+        });
+    
+}
+
+function openMobileMenu() {
+    /**
+     * openMobileMenu reveals the mobile navigation bar containing hyperlinks to other web-pages.
+    */
+   
+    let timeOutDelay = 100;
+
+    // Change the display of $MOBILE_NAV from 'none' to 'flex'.
+    $MOBILE_NAV.classList.remove('mobile-nav__hide');
+    $MOBILE_NAV.classList.add('mobile-nav__open');
+
+    /* 
+    Hide the brand and make it unclickable.
+    By setting the opacity of the brand to '0' and removing hover events, rather than setting its display to 'none', the position of the toggle button can still be 
+    maintained in the header flexbox container in relation to the brand.
+    */
+    $BRAND.classList.add('hidden');
+    $BRAND.classList.add('unclickable');
+
+    // Execute transition in of menu background AFTER the mobile-nav's display is changed from 'none' (otherwise it will not execute).
+    setTimeout(() => {
+        $MOBILE_NAV_BG.classList.add('mobile-nav__background--open');  
+        // Reset the header as clicking the toggle button to open the mobile menu scrolls the user to the top of the current page.
+        resetHeader();
+        // Trigger transition in of menu items.
+        $MOBILE_NAV_ITEMS.classList.add('mobile-nav__items--reveal');
+    }, timeOutDelay);
+
+    // To prevent scrolling on the body while the mobile menu is open. This will also cause the page to automatically scroll to the top.
+    $BODY.style.position = 'fixed';
+}
+
+function resetHeader() {
+    /**
+     * Resets the colours and state of the header and its elements as it was at the top of the page.
+     */
+
+    // Turn header background transparent
+    $MAIN_HEADER.classList.add('main-header--transparent');
+    $MAIN_HEADER.classList.remove('header__element--white');
+    // Add alternate (white) logo
+    $LOGO_ALT.classList.remove('brand-alt-hover--disable');
+    $LOGO_ALT.classList.add('image_on');
+    $LOGO.classList.remove('brand-hover--disable');
+    $LOGO.classList.add('image_off');
+}
+
+function closeMobileMenu() {
+    /**
+     * closeMobileMenu hides the mobile navigation bar.
+    */
+
+    let timeOutDelay = 500;
+   
+    // Execute transition that hides the links.
+    $MOBILE_NAV_ITEMS.classList.remove('mobile-nav__items--reveal');
+    $MOBILE_NAV_ITEMS.classList.add('mobile-nav__items--hide');
+
+    // Execute transition that hides the mobile menu background.
+    $MOBILE_NAV_BG.classList.add('mobile-nav__background--close');
+    $MOBILE_NAV_BG.classList.remove('mobile-nav__background--open');
+
+    // AFTER the transitions are done
+    setTimeout(() => {
+        // Set display of the mobile nav-bar menu to 'none'
+        $MOBILE_NAV.classList.remove('mobile-nav__open');
+        $MOBILE_NAV.classList.add('mobile-nav__hide');
+
+        // Remove the classes added to animate the closure of the items and background.
+        $MOBILE_NAV_ITEMS.classList.remove('mobile-nav__items--hide');
+        $MOBILE_NAV_BG.classList.remove('mobile-nav__background--close');
+    }, timeOutDelay);
+
+    // Make brand clickable again.
+    $BRAND.classList.remove('hidden');
+    $BRAND.classList.remove('unclickable');
+
+    // Make body scroll-able again.
+    $BODY.style.position = 'static';
+}
+
+function openToggleAnimation() {
+    /**
+     * openToggleAnimation executes animation of toggle button menu hamburger turning into 'x' exit button.
+     */
+    for (let i=0; i<3; i++) {
+        $TOGGLE_BTN_BARS[i].classList.add('header__element--grey');
+        $TOGGLE_BTN_BARS[i].classList.remove('header__element--white');
+        $TOGGLE_BTN_BARS[i].classList.add(`toggle-button__bar-${i+1}--clicked`);
+    }
+
+    // Change label of toggle button to indicate it can be clicked on to close the mobile menu.
+    $MENU_LABEL.textContent = "CLOSE";
+}
+
+function closeToggleAnimation() {
+    /**
+     * closeToggleAnimation executes animation of 'x' exit button turning into toggle button menu hamburger.
+     */
+
+    let timeOutDelay = 500;
+
+    for (let i=0; i<3; i++) {
+        $TOGGLE_BTN_BARS[i].classList.remove('header__element--grey');   
+        $TOGGLE_BTN_BARS[i].classList.add('header__element--white');
+        $TOGGLE_BTN_BARS[i].classList.remove(`toggle-button__bar-${i+1}--clicked`);
+        $TOGGLE_BTN_BARS[i].classList.add(`toggle-button__bar-${i+1}--clicked-off`);
+    }
+
+    // Remove animation classes after animations are complete
+    setTimeout(() => {
+        for (let i=0; i<3; i++) {
+            $TOGGLE_BTN_BARS[i].classList.remove(`toggle-button__bar-${i+1}--clicked-off`);
+        }
+    }, timeOutDelay)
+    // Change label of toggle button to indicate it can be clicked on to open the mobile menu.
+    $MENU_LABEL.textContent = "MENU";
 }
